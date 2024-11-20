128efc63721381e4409e9167f62bd12b35d6e292
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-11-20 09:19:13 +0100

    COMMIT

--- docs/main.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.getElementById('btnMenu');
-const menu = document.getElementById('menu');
-
-btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
